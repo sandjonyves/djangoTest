5328905633a2bc71f79c380abea157bc4dad74f6
sandjonyves <sandjonyves@gmail.com>
2023-08-20 02:52:10 +0200

    test6

--- backend/urls.py
+++ backend/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('data/', include('frontend.urls')),
+    path('Data/', include('frontend.urls')),
     path('',views.index)
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
