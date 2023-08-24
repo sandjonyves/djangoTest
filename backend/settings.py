a8a8d07e524928dd31e176bd4ac179c2294c9ff5
sandjonyves <sandjonyves@gmail.com>
2023-08-24 05:04:20 +0200

    test22

--- backend/settings.py
+++ backend/settings.py
@@ -146,12 +146,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/' 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = [
+# STATICFILES_DIRS = [
    
-    os.path.join(BASE_DIR,'build','static'), 
-]
+#     os.path.join(BASE_DIR,'build','static'), 
+# ]
 
 
 MEDIA_URL = '/media/'
--- backend/views.py
+++ backend/views.py
@@ -3,9 +3,9 @@ from django.views.decorators.cache import never_cache
 from django.shortcuts import render,HttpResponse
 
 
-index = never_cache(TemplateView.as_view(template_name = '/home/sandjon/Web/django/backend/build/index.html'))
-# def index (request) :
-#     ct = {
-#         "fedfdfdf"
-#     }
-#     return HttpResponse('fdffdfdfdffddfdfd')
+# index = never_cache(TemplateView.as_view(template_name = '/home/sandjon/Web/django/backend/build/index.html'))
+def index (request) :
+    ct = {
+        "fedfdfdf"
+    }
+    return HttpResponse('fdffdfdfdffddfdfd')
